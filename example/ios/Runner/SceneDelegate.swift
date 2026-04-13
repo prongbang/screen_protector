6f457c75ea2932343efdf980d4d2f86e89428285
Malo Léon <malo.leon@wyatt-studio.fr>
2026-04-13 16:59:57 +0200

    chore: update project to support UISceneDelegate

--- example/ios/Runner/SceneDelegate.swift
+++ example/ios/Runner/SceneDelegate.swift
@@ -8,28 +8,42 @@
 import UIKit
 import Flutter
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    var window: UIWindow?
-    
-    func scene(_ scene: UIScene,
-               willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
-        
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        guard let flutterEngine = appDelegate.flutterEngine else { return }
-        
+class SceneDelegate: FlutterSceneDelegate {
+    let flutterEngine = FlutterEngine(name: "my_engine")
+
+    override func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        flutterEngine.run()
+        GeneratedPluginRegistrant.register(with: flutterEngine)
+        self.registerSceneLifeCycle(with: flutterEngine)
+
         let flutterViewController = FlutterViewController(
             engine: flutterEngine,
             nibName: nil,
             bundle: nil
         )
-        
+
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = flutterViewController
         self.window = window
         window.makeKeyAndVisible()
+
+        super.scene(scene, willConnectTo: session, options: connectionOptions)
+    }
+
+    override func sceneDidEnterBackground(_ scene: UIScene) {
+        let application = UIApplication.shared
+        var bgTask: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0)
+        bgTask = application.beginBackgroundTask {
+            application.endBackgroundTask(bgTask)
+            bgTask = UIBackgroundTaskIdentifier.invalid
+        }
+        super.sceneDidEnterBackground(scene)
     }
 }
 
